2078453b8a933a3278a8008863ae5d474b24dd73
matczarch <matteo.richard95@gmail.com>
2021-02-19 19:02:35 +0100

    Version final

--- master.js
+++ master.js
@@ -22,6 +22,7 @@ var animation = bodymovin.loadAnimation({
 })
 
 // random reponse
+var essai; 
 var sequences = [
    [543,1049],
    [1129,1625],
@@ -34,16 +35,15 @@ var sequences = [
    [5221,5729],
    [5802,6309],
 
-
-
-
 ];
 
 function animation_a_hasard () {
    var sequence = sequences[ Math.floor( Math.random() * (sequences.length - 1) ) ];
-   console.info( 'séquence sélectionnée', sequence );
-   return sequence;
+   console.info('séquence sélectionnée', sequence);
+   essai = sequence; 
+   return sequence, essai;
 }
+
 
 animation.addEventListener( 'DOMLoaded', function () {
     animation.playSegments([0,326],true);
@@ -52,10 +52,9 @@ animation.addEventListener( 'DOMLoaded', function () {
 
       // si on est entre la frame 100 ET la frame 400 incluses
       // ou plutôt : si la frame actuelle >= 100 et <= 400
-      if ( animation.currentFrame >= 300 && animation.currentFrame <= 325 ) {
-         console.info('vitesse : 2x');
+      if ( animation.currentFrame >= 180 && animation.currentFrame <= 325 ) {
          animation.playSegments([ [326,542] , animation_a_hasard() ],true);
-
+         console.info(essai);
 
       // autre test si le premier n'est pas vrai
       // si la frame courant est >= 0 ET <= 50
@@ -68,6 +67,198 @@ animation.addEventListener( 'DOMLoaded', function () {
    });
 } )
 
+if (essai = [543,1049]) {
+animation.addEventListener( 'DOMLoaded', function () {
+   animation.playSegments([0,325],true);
+  animation.wrapper.addEventListener('click', function () {
+     console.log(animation.currentFrame)
+
+     // si on est entre la frame 100 ET la frame 400 incluses
+     // ou plutôt : si la frame actuelle >= 100 et <= 400
+     if ( animation.currentFrame >= 150 && animation.currentFrame <= 508) {
+        console.info('vitesse : 2x');
+        animation.playSegments([0,325],true);
+
+
+     // autre test si le premier n'est pas vrai
+     // si la frame courant est >= 0 ET <= 50
+     // OU (||)
+     // si entre 550 et 600 incluses
+     } else if (false) {
+        console.info('restart');
+     
+     }
+  });
+} )
+}
+else if (essai  = [1129,1625]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
+else if (essai  == [1712,2221]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
+else if (essai  = [2296,2802]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
+else if (essai  = [2893,3400]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
+else if (essai  = [4652,5160]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
+else if (essai  = [5221,5729]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
+else if (essai  = [5802,6309]) {
+   animation.addEventListener( 'DOMLoaded', function () {
+      animation.playSegments([0,325],true);
+     animation.wrapper.addEventListener('click', function () {
+        console.log(animation.currentFrame)
+   
+        // si on est entre la frame 100 ET la frame 400 incluses
+        // ou plutôt : si la frame actuelle >= 100 et <= 400
+        if ( animation.currentFrame >= 150 && animation.currentFrame <= 505) {
+           console.info('vitesse : 2x');
+           animation.playSegments([0,325],true);
+   
+   
+        // autre test si le premier n'est pas vrai
+        // si la frame courant est >= 0 ET <= 50
+        // OU (||)
+        // si entre 550 et 600 incluses
+        } else if (false) {
+           console.info('restart');
+        
+        }
+     });
+   } )
+}
 
 
 
@@ -75,89 +266,4 @@ animation.addEventListener( 'DOMLoaded', function () {
 
 
 
-//boutons
-  var mon_bouton = document.getElementById( 'bouton-lire' );
 
-  mon_bouton.addEventListener( 'click', function () {
-   animation.play();
-})
-
-var mon_bouton = document.getElementById( 'bouton-pause' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.pause();
-})
-
-var mon_bouton = document.getElementById( 'bouton-1' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 0,326 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-2' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 327,542 ], true );
-   sequence;
-
-})
-
-var mon_bouton = document.getElementById( 'bouton-3' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 543,1049 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-4' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 1129,1625 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-5' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 1712,2221 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-6' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 2296,2802 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-7' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 2893,3400 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-8' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 3489,3996 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-9' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 4106,4612 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-10' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 4652,5160 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-11' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 5221,5729 ], true );
-})
-
-var mon_bouton = document.getElementById( 'bouton-12' );
-
-  mon_bouton.addEventListener( 'click', function () {
-   animation.playSegments( [ 5802,6309 ], true );
-})
